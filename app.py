24912e5db9fa9b6572eae8f2d13f59a9e0f1d446
bungo <bungousui@gmail.com>
2025-01-19 07:29:30 -0800

    20250119DellDT

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect # type: ignore
+from wtforms import StringField, Form, SubmitField, IntegerField, PasswordField, DateField, RadioField, BooleanField # type: ignore
+from wtforms.form import Form # type: ignore
+import os
+
 
 app=Flask(__name__)
+
+app.config['SECRET_KEY']="mysecretkey"
+print(os.urandom(16))
 
 players = [
     {'id': 1, 'name': 'Joey', 'age': 12, 'image': 'Joey.jpg'},
@@ -8,6 +15,15 @@ players = [
     {'id': 3, 'name': 'Rose', 'age': 16, 'image': 'Rose.jpg'},
     {'id': 4, 'name': 'Joy', 'age': 11, 'image': 'Joy.jpg'},
 ]
+
+class UserForm(Form): 
+    name=StringField('Name')
+    age=IntegerField('Age')
+    password=PasswordField('Password')
+    birthday=DateField('Birthday')
+    gender=RadioField('Gender', choices=[('man','male'), ('woman','Female')])
+    experience=BooleanField('Have Experience?')
+    submit=SubmitField('Submit')
 
 @app.route('/')
 def index():
@@ -29,6 +45,27 @@ def player_detail(player_id):
 @app.route('/terms')
 def terms():
     return render_template('terms.html')
+
+@app.route('/form_registration' ,methods=['GET', 'POST'])
+def registration_form():
+    name = age = password = birthday = gender = experience = ''
+    form=UserForm(request.form)
+    if request.method == "POST":
+        if form.validate():
+            name=form.name.data
+            age=form.age.data
+            password=form.password.data
+            birthday=form.birthday.data
+            gender=form.gender.data
+            experience=form.experience.data
+            submit=form.submit.data
+            form=UserForm()
+            print('登録完了しました')
+            print(form.name())
+        else :
+            print('違います')
+    return render_template('form_registration.html', form=form, name=name, age=age, password=password, birthday=birthday, gender=gender, experience=experience)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
